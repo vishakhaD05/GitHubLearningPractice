83ec192782c9d2efb30942dd51b9753d1cf3f7c0
2122571 <2122571@LTIN423952.cts.com>
2024-04-01 15:48:54 +0530

    Update Changes

---
+++ GitHub_Practice/src/GitHub_Learning/Addition.java
@@ -0,0 +1,15 @@
+package GitHub_Learning;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int c =add(4, 5);
+		System.out.println("Sum="+ c);
+
+	}
+	public static int add(int a, int b)
+	{
+		return (a+b);
+	}
+
+}
---
+++ GitHub_Practice/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 2122571
+ *
+ */
+module GitHub_Practice {
+}
